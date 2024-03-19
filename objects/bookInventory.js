e5c0ab6b05d1c284cb636d76189089f402be7fd6
kuma <kumaleta2021@gmail.com>
2024-03-19 23:39:17 +0300

    book inventory object is added

---
+++ objects/bookInventory.js
@@ -0,0 +1,7 @@
+const bookInventory={
+    tittle:" oromay",
+    author:"bealu girma",
+    pages:123,
+    available:true
+}
+console.log(bookInventory)
---
+++ objects/userProfile.js
@@ -0,0 +1,7 @@
+const userProfile={
+    name:"kuma",
+    age:21,
+    email:"kumaleta2021@gmail.com",
+
+}
+console.log("name :"+userProfile.name+ " \n age:"+userProfile.age+  "\n email:"+userProfile.email)
